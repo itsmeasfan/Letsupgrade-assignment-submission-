21ff5691b2c817bbc91cf280bf35ab7ea382a8d6
itsmeasfan <71650527+itsmeasfan@users.noreply.github.com>
2020-11-11 21:49:12 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("RAINBOW")
+
+
+# In[5]:
+
+
+print("LETS UPGRADE")
+
+
+# In[10]:
+
+
+CP = int(input())
+SP = int(input())
+
+
+# In[11]:
+
+
+if(CP>SP):
+    print("Loss")
+elif(CP<SP):
+    print("Profit")
+elif(CP==SP):
+    print("Neither")
+
+
+# In[14]:
+
+
+euro = int(input())
+rupee = euro * 80
+print(rupee)
+
+
+# In[ ]:
+
+
+
+
